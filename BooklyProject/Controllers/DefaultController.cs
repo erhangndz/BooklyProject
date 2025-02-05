fce36dfe8fb647e0d22bd411be529e3cf55d969c
Erhan Gündüz <erhangndz@live.com>
2025-02-05 21:11:46 +0300

    updated

---
+++ BooklyProject/Controllers/DefaultController.cs
@@ -0,0 +1,32 @@
+﻿using BooklyProject.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BooklyProject.Controllers
+{
+    public class DefaultController : Controller
+    {
+        BooklyContext context = new BooklyContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult DefaultCategories()
+        {
+            var values = context.Categories.OrderByDescending(x=>x.CategoryId).Take(6).ToList();
+
+            return PartialView(values);
+        }
+
+
+        public PartialViewResult DefaultBooks()
+        {
+            var values = context.Books.OrderByDescending(x=>x.BookId).Take(6).ToList();
+            return PartialView(values);
+        }
+    }
+}
