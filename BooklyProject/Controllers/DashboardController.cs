f9e991679d4e2f37cba0be1a144a88f9a24d78dd
Erhan Gündüz <erhangndz@live.com>
2025-02-12 20:54:48 +0300

    updated

---
+++ BooklyProject/Controllers/DashboardController.cs
@@ -0,0 +1,29 @@
+﻿using BooklyProject.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BooklyProject.Controllers
+{
+    public class DashboardController : Controller
+    {
+        BooklyContext context = new BooklyContext();
+        public ActionResult Index()
+        {
+            ViewBag.bookCount = context.Books.Count();
+            ViewBag.categoryCount = context.Categories.Count();
+            ViewBag.authorCount = context.Authors.Count();
+            ViewBag.testimonialCount = context.Testimonials.Count();
+
+            ViewBag.avgPrice = context.Books.Average(x => x.Price).ToString("000.00");
+
+            ViewBag.mostExpensiveBook = context.Books.OrderByDescending(x=>x.Price).Select(x=>x.BookName).FirstOrDefault();
+            ViewBag.cheapestBook = context.Books.OrderBy(x => x.Price).Select(x => x.BookName).FirstOrDefault();
+            ViewBag.onSaleBookCount = context.Books.Where(x => x.IsOnSale).Count();
+
+            return View();
+        }
+    }
+}
